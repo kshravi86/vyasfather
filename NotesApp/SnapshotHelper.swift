924077494f3d3c4713eeca6f5df0ba429c149ff7
Aakash <kshravi86@gmail.com>
2025-10-24 16:16:43 +0530

    Fix: Update SnapshotHelper.swift boilerplate to resolve 'outdated helper file' warning.

--- NotesApp/SnapshotHelper.swift
+++ NotesApp/SnapshotHelper.swift
@@ -3,10 +3,6 @@ import XCTest
 
 @objc public class Snapshot: NSObject {
     @objc public class func setupFastlaneSnapshot() {
-        // This is called before the UI test starts.
-        // You can put any setup code here.
-        
-        // Set a launch argument to indicate that the app is running in UI Test mode
         let app = XCUIApplication()
         app.launchArguments.append("-FASTLANE_SNAPSHOT")
         app.launch()
